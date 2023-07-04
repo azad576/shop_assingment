c968e9ec8086196f41819bc52367f6496bfecd53
azad <md1azad0@gmail.com>
2023-07-05 00:24:47 +0600

    again

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const product_details = [
+  {
+    productdate: "new",
+    offer: "29% OFF",
+    pimage:
+      "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg",
+    product_name: "Porto Headphone",
+    category: "ACCESSORIES",
+    price: "$188",
+    celprice: "$170",
+  },
+  {
+    productdate: "new",
+    offer: "29% OFF",
+    pimage:
+      "https://images.pexels.com/photos/821651/pexels-photo-821651.jpeg?cs=srgb&dl=pexels-alex-andrews-821651.jpg&fm=jpg",
+    product_name: "Porto Headphone",
+    category: "ACCESSORIES",
+    price: "$188",
+    celprice: "$170",
+  },
+];
---
+++ js/main.js
@@ -0,0 +1,8 @@
+const products = document.getElementById("products");
+products.innerText = `<h1>Azad vai</h1>`;
+
+product_details.map((item, index) => {
+  console.log(item.pimage);
+});
+
+// let allproduct = "";
